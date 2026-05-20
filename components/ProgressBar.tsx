1bbcff8877f16c2efbbc2343b6972abab883c0a2
Monik <monik@insighttimer.com>
2026-05-20 16:09:04 +0530

    fix: build issue

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -8,10 +8,10 @@ export function ProgressBar() {
   const key = `${pathname}?${searchParams.toString()}`;
 
   return (
-    <div className="fixed left-0 top-0 z-[60] h-1 w-full bg-transparent">
+    <div className="fixed left-0 top-0 z-60 h-1 w-full bg-transparent">
       <div
         key={key}
-        className="route-progress h-full bg-gradient-to-r from-cyan-300 via-pink-400 to-lime-300"
+        className="route-progress h-full bg-linear-to-r from-cyan-300 via-pink-400 to-lime-300"
       />
     </div>
   );
